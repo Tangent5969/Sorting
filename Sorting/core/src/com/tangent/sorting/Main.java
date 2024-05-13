23325ddd700dbafbb2f7a56870c6ae6cd992afef
Tangent5969 <82731644+Tangent5969@users.noreply.github.com>
2024-05-13 19:53:56 +0100

    testing things

--- Sorting/core/src/com/tangent/sorting/Main.java
+++ Sorting/core/src/com/tangent/sorting/Main.java
@@ -1,31 +1,52 @@
 package com.tangent.sorting;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	OrthographicCamera mainCamera;
+	OrthographicCamera settingsCamera;
+	ScreenViewport mainViewport;
+	ScreenViewport settingsViewport;
+	ShapeRenderer sr;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		mainCamera = new OrthographicCamera(Gdx.graphics.getWidth() * 0.8f, Gdx.graphics.getHeight());
+		settingsCamera = new OrthographicCamera(Gdx.graphics.getWidth() * 0.2f, Gdx.graphics.getHeight());
+
+		mainViewport = new ScreenViewport(mainCamera);
+		mainViewport.setScreenBounds(0, 0, (int) (Gdx.graphics.getWidth() * 0.8), Gdx.graphics.getHeight());
+
+		settingsViewport = new ScreenViewport(settingsCamera);
+		settingsViewport.setScreenBounds((int) (Gdx.graphics.getWidth() * 0.8), 0, (int) (Gdx.graphics.getWidth() * 0.2), Gdx.graphics.getHeight());
+
+		mainCamera.translate(mainCamera.viewportWidth/2, mainCamera.viewportHeight/2);
+		mainCamera.update();
+		mainViewport.apply();
+		settingsViewport.apply();
+
+		sr = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear(0, 0, 0, 0);
+		sr.begin(ShapeRenderer.ShapeType.Filled);
+		sr.setProjectionMatrix(mainCamera.combined);
+		sr.rect(0, 0, mainCamera.viewportWidth, mainCamera.viewportHeight);
+		sr.end();
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		sr.dispose();
 	}
 }
