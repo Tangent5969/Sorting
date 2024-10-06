263c8c21b096606f4f7494e0c6da6590a8bdaeaf
Tangent5969 <82731644+Tangent5969@users.noreply.github.com>
2024-10-06 19:08:15 +0100

    implemented Odd-Even Sort

--- Sorting/core/src/com/tangent/sorting/sorts/OddEvenSort.java
+++ Sorting/core/src/com/tangent/sorting/sorts/OddEvenSort.java
@@ -19,33 +19,34 @@ public class OddEvenSort extends Sort {
 
     private void sort() {
         boolean swap;
-
         do {
-            swap = false;
-            for (int i = 1; i < arrayController.getLength() - 1; i += 2) {
-                MainController.specialBarsClear();
+            swap = bubble(1, false);
+            swap = bubble(0, swap);
+        }
+        while (swap);
+    }
+
+    private boolean bubble(int start, boolean swap) {
+        for (int i = start; i < arrayController.getLength() - 1; i += 2) {
+            MainController.specialBarsClear();
+            MainController.specialBarsAdd(new IntColourPair(i, Color.RED));
+            Gdx.graphics.requestRendering();
+            checkStatus();
+
+            MainController.specialBarsSet(0, new IntColourPair(i + 1, Color.RED));
+            Gdx.graphics.requestRendering();
+            checkStatus();
+
+            arrayController.addComparisons(1);
+            MainController.audio.playSound(arrayController.getElement(i + 1));
+            if (arrayController.getElement(i) > arrayController.getElement(i + 1)) {
+                arrayController.swap(i, i + 1);
+                swap = true;
                 MainController.specialBarsAdd(new IntColourPair(i, Color.RED));
                 Gdx.graphics.requestRendering();
                 checkStatus();
-
-                MainController.specialBarsSet(0, new IntColourPair(i + 1, Color.RED));
-                Gdx.graphics.requestRendering();
-                checkStatus();
-
-                arrayController.addComparisons(1);
-                MainController.audio.playSound(arrayController.getElement(i + 1));
-                if (arrayController.getElement(i) > arrayController.getElement(i + 1)) {
-                    arrayController.swap(i, i + 1);
-                    swap = true;
-                    MainController.specialBarsAdd(new IntColourPair(i, Color.RED));
-                    Gdx.graphics.requestRendering();
-                    checkStatus();
-                }
             }
-
-
         }
-        while (swap);
-
+        return swap;
     }
 }
