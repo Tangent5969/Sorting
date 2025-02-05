5c79c4f928ba4b2b0ebdf3337df853a95aefaa6f
Tangent5969 <82731644+Tangent5969@users.noreply.github.com>
2025-02-05 21:24:05 +0000

    updated image file handling

--- Sorting/desktop/src/com/tangent/sorting/DesktopLauncher.java
+++ Sorting/desktop/src/com/tangent/sorting/DesktopLauncher.java
@@ -7,28 +7,29 @@ import com.tangent.sorting.controls.Main;
 import com.tangent.sorting.controls.MainController;
 import com.tangent.sorting.ui.visual.Image;
 
+import javax.imageio.IIOException;
 import java.io.IOException;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Sound Of Sorting");
-		config.setWindowIcon("icon.png");
-		config.setForegroundFPS(60);
-		config.setWindowSizeLimits(640, 480, -1, -1);
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Sound Of Sorting");
+        config.setWindowIcon("icon.png");
+        config.setForegroundFPS(60);
+        config.setWindowSizeLimits(640, 480, -1, -1);
 
-		config.setWindowListener(new Lwjgl3WindowAdapter() {
-			@Override
-			public void filesDropped(String[] files) {
-				try {
-					Image.selectImage(files[0]);
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				} catch (NullPointerException e) {
-					MainController.setErrorCode(MainController.Error.UnsupportedImage);
-				}
-			}
-		});
-		new Lwjgl3Application(new Main(), config);
-	}
+        config.setWindowListener(new Lwjgl3WindowAdapter() {
+            @Override
+            public void filesDropped(String[] files) {
+                try {
+                    Image.selectImage(files[0]);
+                } catch (IIOException | NullPointerException e) {
+                    MainController.setErrorCode(MainController.Error.UnsupportedImage);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        new Lwjgl3Application(new Main(), config);
+    }
 }
